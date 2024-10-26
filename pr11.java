c803041ca2516fb3999e287c95e3b7ca767fe429
Alokanyaani <alokanyaani@gmail.com>
2024-10-27 02:20:48 +0530

    java work

---
+++ pr11.java
@@ -0,0 +1,11 @@
+public class pr11 {
+    public static void main(String[] args){
+        
+        int A = 24;
+
+        String type =((A % 2 )==0)? "EVEN" : "ODD";
+        System.out.println(type);
+    }
+
+    
+}
