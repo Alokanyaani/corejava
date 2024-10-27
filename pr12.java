be3c0ff617cf34e6b44f6a96722d3edb0cb80e5a
Alokanyaani <alokanyaani@gmail.com>
2024-10-27 16:14:11 +0530

    java work

---
+++ pr12.java
@@ -0,0 +1,10 @@
+public class pr12 {
+    public static void main(String[] args){
+        int marks = 32;
+
+        String marksheet = marks >= 33 ? "PASS"  : "FAIL";
+        System.out.println(marksheet);
+         
+    }
+    
+}
