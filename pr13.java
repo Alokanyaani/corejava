6f736c8ed0eed19a75caee382a5f2639cb10cc5c
Alokanyaani <alokanyaani@gmail.com>
2024-10-29 21:42:37 +0530

    java work

---
+++ pr13.java
@@ -0,0 +1,24 @@
+
+
+
+
+public class pr13 {
+    public static void main(String[] args){
+   
+     
+
+        int  num = 2;
+
+        switch(num){
+            case 1: System.out.println("Samosa");
+                   break;
+            case 2: System.out.println("Burger");
+                   break;
+            case 3: System.out.println("Mango Shake");
+                   break;   
+            default: System.out.println("wake up");          
+
+        }
+    }
+    
+}
