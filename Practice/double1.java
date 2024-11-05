f2b6cf827aaa35c521cba929429e53af40357103
Alokanyaani <alokanyaani@gmail.com>
2024-11-05 21:08:44 +0530

    java work

---
+++ Practice/double1.java
@@ -0,0 +1,11 @@
+//Write a Java program to compute a specified formula
+
+package Practice;
+
+public class double1 {
+    public static void main(String[] args){
+        double A = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+        System.out.println(A);
+    }
+    
+}
