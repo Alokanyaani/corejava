a355190e6f6d0b9578652cbd020360b9e1d50a38
Alokanyaani <alokanyaani@gmail.com>
2024-11-05 21:12:19 +0530

    java work

---
+++ Practice/double2.java
@@ -0,0 +1,11 @@
+//Write a Java program to compute the specified expressions and print the output
+
+package Practice;
+
+public class double2 {
+    public static void main(String[] args){
+        double  A = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+        System.out.println(A); 
+    }
+    
+}
