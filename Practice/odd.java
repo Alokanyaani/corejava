8196e8a9527466a077a95ba16aa5da83fd66a8d8
Alokanyaani <alokanyaani@gmail.com>
2024-11-18 18:50:30 +0530

    java work

---
+++ Practice/odd.java
@@ -0,0 +1,25 @@
+//Write a Java program to print odd numbers from 1 to 99. Prints one number per line.
+
+
+
+package Practice;
+
+
+
+public class odd {
+    public static void main(String[] args){
+        
+       // int A =sc.nextInt();
+
+        for(int i = 1; i<100; i++){
+            if (i %2 !=0){
+                System.out.println(i);
+        
+            }
+        }
+      
+        
+
+    }
+    
+}
