7529e657286f5c8e4cbafc4e822b7268988476ce
Alokanyaani <alokanyaani@gmail.com>
2024-11-18 10:48:08 +0530

    java work

---
+++ Practice/hello.java
@@ -0,0 +1,13 @@
+//How many times 'Hello' is printed?
+
+
+package Practice;
+public class hello {
+    public static void main(String[] args){
+        for(int i=1; i<=5; i++){
+            System.out.println("hello");
+            i+=2;
+        }
+        
+    }
+}
