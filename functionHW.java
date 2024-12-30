34770f816dbc6673f74f9a51eceb04e30845bb5b
Alokanyaani <alokanyaani@gmail.com>
2024-12-31 01:12:25 +0530

    java work

---
+++ functionHW.java
@@ -0,0 +1,11 @@
+public class functionHW {
+    public static void printhelloworld(){
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+
+    public static void main(String[] args) {
+        printhelloworld();//function call
+    }
+}
