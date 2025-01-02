389840e26f53e59701f3644734f4e12d2b7e49af
Alokanyaani <alokanyaani@gmail.com>
2025-01-02 20:28:51 +0530

    java work

---
+++ multiply.java
@@ -0,0 +1,13 @@
+public class multiply {
+    public static int multiplY(int a,int b){
+        int product=a*b;
+        return product;
+    }
+    public static void main(String[] args) {
+        int a=3;
+        int b=7;
+      int prod= multiplY(a,b);
+        System.out.println("a*b= "+prod);
+
+    }
+}
