57d91830ab7196460ef46a5f7ec52ee499b834fd
Alokanyaani <alokanyaani@gmail.com>
2025-01-05 15:25:12 +0530

    java work

--- functionprimecheck.java
+++ functionprimecheck.java
@@ -1,4 +1,4 @@
-public class functionprimecheck {
+ public class functionprimecheck {
     public static boolean isprime(int n){
         for(int i=2;i<=n-1;i++){
             if(n%i==0){
@@ -11,4 +11,4 @@ public class functionprimecheck {
     public static void main(String[] args) {
         System.out.println(isprime(17));
     }
-}
+ }
