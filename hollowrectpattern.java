203900adc4201c855e672c9ff955531631bef1c6
Alokanyaani <alokanyaani@gmail.com>
2025-03-02 00:51:26 +0530

    java work

---
+++ hollowrectpattern.java
@@ -0,0 +1,19 @@
+public class hollowrectpattern {
+    public static void hollow_rectangle(int totrows,int totcolw){
+        for(int i=1;i<=totrows;i++){
+            for(int j=1;j<=totcolw;j++){
+                if( i==1 || i==totrows || j==1 || j==totcolw ){
+                    System.out.print(" * ");
+                }
+                else{
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        hollow_rectangle(4,5);
+
+    }
+}
