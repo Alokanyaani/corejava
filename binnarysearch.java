97eed299a7478b4840eeb6a959cd9401d63f3861
Alokanyaani <alokanyaani@gmail.com>
2025-03-12 18:49:23 +0530

    java work

--- binnarysearch.java
+++ binnarysearch.java
@@ -1,6 +1,6 @@
 public class binnarysearch {
     public static int BinnarySearch(int[] numbers,int key){
-       int  start =0,end =numbers.length;
+       int  start =0,end =numbers.length-1;
 
        while (start<=end){
            int mid=(start+end)/2;
@@ -19,7 +19,7 @@ public class binnarysearch {
     }
     public static void main(String[] args) {
         int numbers[]={2,4,6,8,10,12,14};
-        int key=10;
+        int key=12;
         System.out.println("index is: "+BinnarySearch(numbers,key));
     }
 }
