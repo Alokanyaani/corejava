37c2888decc43b262a38d8a7f58ca6c7acff855a
Alok Kumar <139148871+Alokanyaani@users.noreply.github.com>
2025-05-16 23:11:58 +0530

    Alphabet.java

--- Alphabet.java
+++ Alphabet.java
@@ -4,7 +4,21 @@ import java.util.Scanner;
 
 public class Alphabet {
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter an alphabet: ");
+		char ch=sc.next().charAt(0);
 		
+		if(ch>='A'&&ch<='Z') {
+			System.out.println(ch+" upper case");
+				
+		}else {
+			if(ch>='a'&&ch<='z') {
+				System.out.println(ch+" lower case ");
+		}else {
+			System.out.println(ch+" nither");
+		}
+		}
+		sc.close();
 	}
 
 }
