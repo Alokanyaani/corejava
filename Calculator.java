ff9af2d0ada04ed526cfca1cfd4cdd21e3715473
Alok Kumar <139148871+Alokanyaani@users.noreply.github.com>
2025-05-16 23:17:37 +0530

    java work

--- Calculator.java
+++ Calculator.java
@@ -1,57 +1,58 @@
-package if_else;
-
+package Switch_case;
 import java.util.Scanner;
 
 public class Calculator {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);		
+		System.out.println("enter your choice\n+ for addition\n- for subtraction\n* for multiply\n/ for divide\n% modulus");
+		char choice=sc.next().charAt(0); int a, b , result;
+		switch(choice) {
+		case '+':
+			System.out.println("enter number 1 for addition");
+			a=sc.nextInt();
+			System.out.println("enter number 2 for addition");
+			b=sc.nextInt();
+			result=a+b;
+			System.out.printf("The sum of %d and %d  =  %d",a,b,result);
+			break;
+		case '-':
+			System.out.println("enter number 1 for subtraction");
+			a=sc.nextInt();
+			System.out.println("enter number 2 for subtraction");
+			b=sc.nextInt();
+			result=a-b;
+			System.out.printf("The sub of %d and %d  =  %d",a,b,result);
+			break;
+		case '*':
+			System.out.println("enter number 1 for multiply");
+			a=sc.nextInt();
+			System.out.println("enter number 2 for multiply");
+			b=sc.nextInt();
+			result=a*b;
+			System.out.printf("The multiply of %d and %d  =  %d",a,b,result);
+			break;
+		case '/':
+			System.out.println("enter number 1 for divide");
+			a=sc.nextInt();
+			System.out.println("enter number 2 for divide");
+			b=sc.nextInt();
+			result=a/b;
+			System.out.printf("The divide of %d and %d  =  %d",a,b,result);
+			break;
+		case '%':
+			System.out.println("enter number 1 for modulus");
+			a=sc.nextInt();
+			System.out.println("enter number 2 for modulus");
+			b=sc.nextInt();
+			result=a%b;
+			System.out.printf("The modulus of %d and %d  =  %d",a,b,result);
+			break;	
+		default:
+			System.out.println("There is no such option available");
+			break;
 		
-		System.out.println(" enter your choice\n+ for addition\n- for subtraction\n* for multiply\n/ for divide\n% modulus");
-		char ch=sc.next().charAt(0);
-		int num1,num2,result;
-		
-		
-		if(ch=='+') {
-			System.out.println("Enter 1st num: ");
-			num1=sc.nextInt();
-			
-			System.out.println("Enter 1st num: ");
-			num2=sc.nextInt();
-			result =num1+num2;
-			System.out.printf("The sum of %d and %d  =  %d",num1,num2,result);
-		}else if(ch=='-') {
-			System.out.println("Enter 1st num: ");
-			num1=sc.nextInt();
-			
-			System.out.println("Enter 1st num: ");
-			num2=sc.nextInt();
-			result =num1-num2;
-			System.out.printf("The subtraction of %d and %d  =  %d",num1,num2,result);
-		}else if(ch=='*') {
-			System.out.println("Enter 1st num: ");
-			num1=sc.nextInt();
-			
-			System.out.println("Enter 1st num: ");
-			num2=sc.nextInt();
-			result =num1*num2;
-			System.out.printf("The multiply of %d and %d  =  %d",num1,num2,result);
-		}else if(ch=='/') {
-			System.out.println("Enter 1st num: ");
-			num1=sc.nextInt();
-			
-			System.out.println("Enter 1st num: ");
-			num2=sc.nextInt();
-			result =num1/num2;
-			System.out.printf("The divide of %d and %d  =  %d",num1,num2,result);
-		}else if(ch=='%') {
-			System.out.println("Enter 1st num: ");
-			num1=sc.nextInt();
-			
-			System.out.println("Enter 1st num: ");
-			num2=sc.nextInt();
-			result =num1%num2;
-			System.out.printf("The modulus of %d and %d  =  %d",num1,num2,result);
 		}
+	  
 		sc.close();
 	}
 
